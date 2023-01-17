cc7b67afdf59c206cfe92e8a41cca501596a009c
LPSS063\Simian <agung.eko.wibowo@gmail.com>
2023-01-17 18:18:21 +0700

    perbaiki backimg login

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,21 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import LoginView from './views/login/Login.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    
+<template>  
+  <main class="back">
+    <LoginView></LoginView>  
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style>
+  .back{
+    background-image: url('./assets/back1.jpg');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/views/login/Login.vue
@@ -0,0 +1,37 @@
+<script>
+    export default{
+        data(){
+            return {
+                username:'',
+                password:''   
+            }
+        }
+    }
+</script>
+<template>
+    <!-- <div class="container-fluid vh-100 d-flex justify-content-center">
+        <div class="d-flex align-item-center w-75">
+           <div class="border border-dark w-100 ">test</div>
+        </div>
+    </div> -->
+    <div class="vh-100 d-flex align-items-center justify-content-center">
+        <div class="w-75 border border-secondary border-2 rounded backimg">
+            <div class="row">
+                <div class="col-8 ">
+                    <img src="./back.jpg" class="backimg w-100 p-1" />
+                </div>
+                <div class="col-4">
+                    <div class="row">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .backimg  {
+        background-color: white;
+    }
+</style>
